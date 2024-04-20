ff15484af752fa9753be5a6a9c57926c20463246
Rohit Singla <rohitdeepu17@gmail.com>
2024-04-20 11:37:53 +0530

    added daos

---
+++ app/src/main/java/com/example/advaitassignment/data/local/dao/UnsplashImageDao.kt
@@ -0,0 +1,20 @@
+package com.example.advaitassignment.data.local.dao
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import com.example.advaitassignment.model.UnsplashImage
+
+@Dao
+interface UnsplashImageDao {
+    @Query("SELECT * FROM unsplash_image_table")
+    fun getAllImages(): PagingSource<Int, UnsplashImage>
+
+    @Insert(onConflict = REPLACE)
+    suspend fun addImages(images: List<UnsplashImage>)
+
+    @Query("DELETE FROM unsplash_image_table")
+    suspend fun deleteAllImages()
+}
