b1e195c8ed2dba79e0f37a28b12b4f79fe23ea4d
Rohit Singla <rohitdeepu17@gmail.com>
2024-04-20 17:51:19 +0530

    added Repository

---
+++ app/src/main/java/com/example/advaitassignment/data/repository/Repository.kt
@@ -0,0 +1,33 @@
+package com.example.advaitassignment.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.advaitassignment.data.local.UnsplashDatabase
+import com.example.advaitassignment.data.paging.UnsplashRemoteMediator
+import com.example.advaitassignment.data.remote.UnsplashApi
+import com.example.advaitassignment.model.UnsplashImage
+import com.example.paging3demo.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class Repository @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val unsplashDatabase: UnsplashDatabase
+) {
+
+    fun getAllImages(): Flow<PagingData<UnsplashImage>> {
+        val pagingSourceFactory = { unsplashDatabase.unsplashImageDao().getAllImages() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = UnsplashRemoteMediator(
+                unsplashApi = unsplashApi,
+                unsplashDatabase = unsplashDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+}
