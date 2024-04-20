39850e20568964b64e55271a2f77b6a4d5499cba
Rohit Singla <rohitdeepu17@gmail.com>
2024-04-20 17:19:10 +0530

    fixed build errors

--- app/src/main/java/com/example/advaitassignment/data/local/dao/UnsplashImageDao.kt
+++ app/src/main/java/com/example/advaitassignment/data/local/dao/UnsplashImageDao.kt
@@ -3,7 +3,6 @@ package com.example.advaitassignment.data.local.dao
 import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import com.example.advaitassignment.model.UnsplashImage
 
@@ -12,7 +11,7 @@ interface UnsplashImageDao {
     @Query("SELECT * FROM unsplash_image_table")
     fun getAllImages(): PagingSource<Int, UnsplashImage>
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
     suspend fun addImages(images: List<UnsplashImage>)
 
     @Query("DELETE FROM unsplash_image_table")
